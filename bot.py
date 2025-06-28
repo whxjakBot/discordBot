1c8b8e0280ab0ac170b5123e9a5b3b669fde8b8b
whxjakBot <whxjak8@gmail.com>
2025-06-28 19:56:28 -0400

    Subo bot con keep_alive para Render

--- bot.py
+++ bot.py
@@ -11,11 +11,13 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 if TOKEN is None:
     raise ValueError("❌ No se encontró la variable DISCORD_TOKEN en el archivo .env.")
 
-# Crear instancia del bot
+# Configurar intents
 intents = discord.Intents.all()
+
+# Crear instancia del bot
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-# Eliminar el comando help por defecto para evitar conflicto al cargar tu propio 'cogs.help'
+# Eliminar el comando help por defecto (opcional)
 bot.remove_command("help")
 
 # Lista de extensiones (cogs) a cargar
@@ -25,7 +27,7 @@ initial_extensions = [
     'cogs.seguridad'
 ]
 
-# Función asincrónica para cargar extensiones
+# Cargar extensiones
 async def load_extensions():
     for extension in initial_extensions:
         try:
@@ -34,13 +36,17 @@ async def load_extensions():
         except Exception as e:
             print(f'❌ Error al cargar {extension}: {e}')
 
-# Evento cuando el bot esté listo
+# Evento on_ready
 @bot.event
 async def on_ready():
     print(f'🤖 Bot conectado como {bot.user}')
 
-# Función principal que ejecuta el bot
+# keep_alive para Render
+from keep_alive import keep_alive
+
+# Función principal
 async def main():
+    keep_alive()  # mantener activo el bot en Render
     async with bot:
         await load_extensions()
         await bot.start(TOKEN)
